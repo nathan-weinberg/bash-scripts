156c6488cea04e10a308f5a97e3e857572ede6fa
Nathan Weinberg <nathan2@stwmd.net>
2018-07-24 10:59:49 -0400

    Added support for Fedora OS

--- update.sh
+++ update.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-echo "##########################################"
-echo "Last Updated:" $(date)
-echo "##########################################"
-sudo apt-get -y update
-echo "###########################"
-echo "Updated System Package List"
-echo "###########################"
-sudo apt-get -y dist-upgrade
-echo "##############################################"
-echo "Upgraded installed packages to latest versions"
-echo "##############################################"
-sudo apt-get -y install openssh-server
-echo "###################"
-echo "Upgraded ssh server"
-echo "###################"
+DISTRO=$(lsb_release -is)
+
+if [ $DISTRO == 'Raspbian' ]; then
+	echo "##########################################"
+	echo "Last Updated:" $(date)
+	echo "##########################################"
+
+	sudo apt-get -y update
+	echo "###########################"
+	echo "Updated System Package List"
+	echo "###########################"
+	sudo apt-get -y dist-upgrade
+	echo "##############################################"
+	echo "Upgraded installed packages to latest versions"
+	echo "##############################################"
+	sudo apt-get -y install openssh-server
+	echo "###################"
+	echo "Upgraded ssh server"
+	echo "###################"
+
+elif [ $DISTRO == 'Fedora' ]; then
+	echo "##########################################"
+	echo "Last Updated:" $(date)
+	echo "##########################################"
+
+	sudo yum -y update
+
+else
+    echo "Unknown Distro: $DISTRO"
+
+fi
