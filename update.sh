88e44c811a603a718e2760f4bd2a94d140ef6ca3
nathanweinberg <nathan2@stwmd.net>
2018-11-26 14:36:21 -0500

    Changed ordering of displays

--- update.sh
+++ update.sh
@@ -3,36 +3,33 @@
 source /etc/os-release
 
 if [ $ID == 'raspbian' ] || [ $ID == 'debian' ]; then
+	echo "###########################"
+	echo "Update System Package List"
+	echo "###########################"
+	sudo apt-get -y update
+	echo "##############################################"
+	echo "Upgrade installed packages to latest versions"
+	echo "##############################################"
+	sudo apt-get -y dist-upgrade
+	echo "###################"
+	echo "Upgrade ssh server"
+	echo "###################"
+	sudo apt-get -y install openssh-server
 	echo "##########################################"
 	echo "Last Updated:" $(date)
 	echo "##########################################"
-
-	sudo apt-get -y update
-	echo "###########################"
-	echo "Updated System Package List"
-	echo "###########################"
-	sudo apt-get -y dist-upgrade
-	echo "##############################################"
-	echo "Upgraded installed packages to latest versions"
-	echo "##############################################"
-	sudo apt-get -y install openssh-server
-	echo "###################"
-	echo "Upgraded ssh server"
-	echo "###################"
 
 elif [ $ID == 'fedora' ]; then
+	sudo dnf -y update
 	echo "##########################################"
 	echo "Last Updated:" $(date)
 	echo "##########################################"
 
-	sudo dnf -y update
-
 elif [ $ID == 'rhel' ]; then
+	sudo yum -y update
         echo "##########################################"
         echo "Last Updated:" $(date)
         echo "##########################################"
-
-	sudo yum -y update
 
 else
 	echo "Unknown Distro: $ID"
