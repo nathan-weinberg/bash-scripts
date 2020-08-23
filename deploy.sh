f36a5d7f20f9acbeb102c10695d2e0e45696278f
Nathan Weinberg <nathan2@stwmd.net>
2020-08-23 16:48:45 -0400

    Replaced Sublime Text with VSCode

--- deploy.sh
+++ deploy.sh
@@ -38,10 +38,11 @@ if [ $ID == 'fedora' ]; then
 	# Spotify
 	sudo flatpak install flathub com.spotify.Client
 
-	# Sublime Text 3
-	sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-	sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-	sudo dnf install -y sublime-text
+	# Visual Studio Code
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+	sudo dnf check-update
+	sudo dnf install code
 
 	# VLC
 	sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
