80b02dd1cb86fd91fe6a35317e6282ff7b58cb7d
Alejandro Cervera <96702705+tricantivu@users.noreply.github.com>
2022-07-12 18:32:27 -0500

    Not being a directory does not imply it does not exist

--- backup.sh
+++ backup.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 cd /home/
+
 if [[ ! -d "$1" ]]; then
     echo "Usage: ./backup.sh [user] [optional: path/to/destination] - Creates tar file containing user's home directory"
+
 elif [[ $# -eq 2 ]]; then
 	if [[ ! -d "$2" ]]; then
-		echo "Destination does not exist."
-		echo "Last Backup Attempted: " $(date)
+
+		exit 1
 	else
+
 		sudo tar czf "$1"_home_"$(date +"%m-%d-%Y")".tar.gz "$1"
 		sudo cp "$1"_home_"$(date +"%m-%d-%Y")".tar.gz "$2"
 		sudo rm "$1"_home_"$(date +"%m-%d-%Y")".tar.gz
 		echo "Last Backup Generated:" $(date)
     fi
 else
+
     sudo tar czf "$1"_home_"$(date +"%m-%d-%Y")".tar.gz "$1"
     echo "Last Backup Generated:" $(date)
 fi
